441de27d4e23c0a8e7504e23cd6716e67c4142aa
Preetha Datta <datta.preetha@gmail.com>
2018-05-30 14:51:00 +0530

    BFS + Cycle detection in undirected graph

--- bfs.py
+++ bfs.py
@@ -28,7 +28,7 @@ while (len(queue)!=0):
         if visited[i] == None: #if vertex has not been visited yet
             visited[i] = 0 #marked as visited
             queue.append(i)
-            
+
 flag = False #variable to check whether the graph is disconnected
 
 for i in visited:
@@ -40,3 +40,35 @@ if flag == True:
 
 else:
     print "all nodes of the graph have been traversed"
+
+
+##BFS can also be used to track cycles in an undirected graph##
+
+##following is a function that will do the same##
+parent = [None,None,None,None,None]
+visited = [None,None,None,None,None] #reinitialising visited array for this purpose
+
+def cycle_BFS(array, s ,parent):
+    parent[s] = None #marked the source as visited in the array
+    visited[s] = 0
+
+    queue = deque()
+
+    queue.append(s)
+
+    while (len(queue)!=0):
+        vertex_check = queue.popleft() #deque the element being checked 
+
+        for i in array[vertex_check]:
+            
+            if parent[i] != None: #if vertex already has a parent then it can be reached from another node
+                #This implies that a cycle exists
+                print "Cycle exists"
+                break
+            else:
+                visited[i] = 0 #marked as visited
+                parent[i] = vertex_check
+                queue.append(i)
+
+
+cycle_BFS(adj_list,2,parent)
